38a58792d793aa705bf5fcb0569b93e1e7178117
dagisha2 <getishdagisha@gmail.com>
2022-10-18 11:46:27 -0700

    sqa

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates class and initiates object"""
+
+
+class Square:
+
+    """This is class named Square"""
+    def __init__(self, size=None):
+        self.__size = size
